dcdbaf7ed59dcedf725a30090cc5be6d5b710692
Victor Portela <168148556+VictorPortela01@users.noreply.github.com>
2024-09-30 16:54:04 -0300

    att css

--- src/Components/Login/Login.jsx
+++ src/Components/Login/Login.jsx
@@ -1,23 +1,42 @@
-import {FaUser, FaLock} from 'react-icons/fa'
+import {FaUser, FaLock} from "react-icons/fa";
+
+import { useState } from "react";
+
+import "./Login.css";
 
 const Login = () => {
+  const [username, setUsername] = useState("");
+  const [password, setPassoword] = useState("");
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    
+    alert("Enviando os dados: "+ username + " - " + password)
+  };
+
   return (
     <div className="container">
-        <form action="">
+        <form onSubmit={handleSubmit}>
             <h1>Acesse o sistema</h1>
-            
-            <div>
-                <input type="text" name="text" id="itext" placeholder='Código'/>
+            <div className="input-field">
+                <input type="text" placeholder="Código" onChange={(e) => setUsername(e.target.value)}/>
                 <FaUser className="icon"/>
             </div>
-            <div>
-                <input type="password"  placeholder='Senha'/>
+            <div className="input-field">
+                <input type="password"  placeholder="Senha" onChange={(e) => setPassoword(e.target.value)}/>
                 <FaLock className="icon"/>
             </div>
+            <div className="recall-forget">
+              <label>
+                <input type="checkbox"/>
+                Lembre de mim
+              </label>
+            </div>
             <button>Entrar</button>
+
         </form>
     </div>
-  )
+  );
 }
 
 export default Login
