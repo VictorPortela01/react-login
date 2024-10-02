3897e2712b3d919640ce781d595ae62d2247c0ff
Victor Portela <168148556+VictorPortela01@users.noreply.github.com>
2024-10-02 09:50:27 -0300

    att

--- src/Components/Login/Login.jsx
+++ src/Components/Login/Login.jsx
@@ -1,50 +1,60 @@
-import {FaUser, FaLock} from "react-icons/fa";
-
+import { FaUser, FaLock } from "react-icons/fa";
 import { useState } from "react";
+
 
 import "./Login.css";
 
 const Login = () => {
-   // Estados para armazenar as entradas do usuário
   const [username, setUsername] = useState("");
-  const [password, setPassoword] = useState("");
+  const [password, setPassword] = useState("");
 
-// Função que é chamada quando o formulário é enviado
   const handleSubmit = (event) => {
-    // Impede que a página seja recarregada
     event.preventDefault();
 
-     // Faz o console log das credenciais do usuário
-    alert("Enviando os dados: "+ username + " - " + password)
+    /*Essa parte está dando erro por conta do 'success' não está vinculado a nada ainda. */
+    if (success) {
+      alert("Login bem-sucedido!");
+    } else {
+      alert("Falha no login, verifique suas credenciais.");
+    }
   };
 
   return (
     <div className="container">
-        <form onSubmit={handleSubmit}>
-            <h1>CERVEJARIA</h1> 
-            <h2>ambev</h2>
-            <h1>DISSOBEL</h1>
-            <div className="input-field">
-                <input type="text" placeholder="Código" required value={username} 
-                onChange={(e) => setUsername(e.target.value)}/>
-                <FaUser className="icon"/>
-            </div>
-            <div className="input-field">
-                <input type="password"  placeholder="Senha" required value={password}
-                 onChange={(e) => setPassoword(e.target.value)}/>
-                <FaLock className="icon"/>
-            </div>
-            <div className="recall-forget">
-              <label>
-                <input type="checkbox"/>
-                Recordar dados
-              </label>
-            </div>
-            <button type="submit">Login</button>
-
-        </form>
+      <form onSubmit={handleSubmit}>
+        <h1>CERVEJARIA</h1>
+        <h2>ambev</h2>
+        <h1>DISSOBEL</h1>
+        <div className="input-field">
+          <input
+            type="text"
+            placeholder="Código"
+            required
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
+          <FaUser className="icon" />
+        </div>
+        <div className="input-field">
+          <input
+            type="password"
+            placeholder="Senha"
+            required
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <FaLock className="icon" />
+        </div>
+        <div className="recall-forget">
+          <label>
+            <input type="checkbox" />
+            Recordar dados
+          </label>
+        </div>
+        <button type="submit">Login</button>
+      </form>
     </div>
   );
-}
+};
 
-export default Login
+export default Login;
