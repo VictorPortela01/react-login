72017885c830620c23ffc655539dd0d4b1fe2121
Victor Portela <168148556+VictorPortela01@users.noreply.github.com>
2024-10-04 16:36:01 -0300

    mudança de extensão no private.route.jsx

--- src/Routers/private.routes.js
+++ src/Routers/private.routes.js
@@ -1,6 +1,5 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Redirect, Switch } from "react-router-dom";
-import { useAuth } from "../Contexts/AuthContext";
 import Login from "../Components/Login/Login";
 import Dashboard from "../Routers/Dashboard";
 
