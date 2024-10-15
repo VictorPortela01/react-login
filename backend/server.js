5ca84f705b9e47fa48411d5dc5e869e41772f985
Victor Portela <168148556+VictorPortela01@users.noreply.github.com>
2024-10-15 16:57:18 -0300

    Banco de dados vinculado

--- backend/server.js
+++ backend/server.js
@@ -11,11 +11,11 @@ app.use(express.json());
 
 // Configuração do banco de dados
 const db = mysql.createConnection({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  port : process.env.DB_PORT || 3306,
+  host: 'localhost',
+  user: 'root',
+  password: '',
+  database: 'meu_banco_de_dados',
+  port : 3306,
 });
 
 // Conectar ao banco de dados
